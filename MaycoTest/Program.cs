40271bae34aae393bbf27b438df6009c09d1662c
jportillo <jorge.portillo@chubb.com>
2022-11-16 12:29:45 -0600

    Add project files.

---
+++ MaycoTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MaycoTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 150; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("MaycoDuppslo");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Dupplo");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Mayco");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
